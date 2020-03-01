f03fa0f593ced94f49f313c4b3fe51aad34fa251
Mario Casciaro <mariocasciaro@gmail.com>
2020-03-01 17:00:45 +0000

    - Add callback to write() - Use grunt v1 - Use const and other code refactorings

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var server = require('./lib/server.js'),
-	socket = require('./lib/socket.js'),
-	protocol = require('./lib/protocol.js');
+const server = require('./lib/server.js')
+const socket = require('./lib/socket.js')
+const protocol = require('./lib/protocol.js')
 
-exports.Server = server.Server;
-exports.createServer = server.create;
+exports.Server = server.Server
+exports.createServer = server.create
 
-exports.Protocol = protocol.Protocol;
-exports.createProtocol = protocol.create;
-exports.ProtocolError = protocol.ProtocolError;
+exports.Protocol = protocol.Protocol
+exports.createProtocol = protocol.create
+exports.ProtocolError = protocol.ProtocolError
 
-exports.Socket = socket.Socket;
-exports.createSocket = socket.create;
-exports.createConnection = exports.connect = socket.connect;
+exports.Socket = socket.Socket
+exports.createSocket = socket.create
+exports.createConnection = exports.connect = socket.connect
